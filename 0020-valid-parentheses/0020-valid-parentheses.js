af0554744db767c0e3edcca6263c05767129708f
Tiffany Brown <139157669+tiffanydbrown@users.noreply.github.com>
2025-07-10 12:01:37 -0700

    Time: 1 ms (85.94%), Space: 53.7 MB (97.52%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    const map = {
+        ')' : '(',
+        '}' : '{',
+        ']' : '['
+    };
+
+    for(let char of s) {
+        if(['(', '{', '['].includes(char)) {
+            stack.push(char);
+        } else {
+            if(stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
